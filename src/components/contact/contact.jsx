c6428d2fb135a0d434d000ac48f6b7be83c71619
Ritika Malik <ritikamalik100102@gmail.com>
2022-09-07 23:59:23 +0530

    contact animation

--- src/components/contact/contact.jsx
+++ src/components/contact/contact.jsx
@@ -3,7 +3,16 @@ import './contact.css'
 
 const Contact = () => {
   return (
-    <div>
+    <div className="teams">
+		 <div class='light x1'></div>
+  <div class='light x2'></div>
+  <div class='light x3'></div>
+  <div class='light x4'></div>
+  <div class='light x5'></div>
+  <div class='light x6'></div>
+  <div class='light x7'></div>
+  <div class='light x8'></div>
+  <div class='light x9'></div>
       <div className="sectionteam">
 				<div className="team">
 					<div className="single-person">
