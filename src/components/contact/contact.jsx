56d95048387954988b2dc2e29f2a214d43c1e93b
Ritika Malik <ritikamalik100102@gmail.com>
2022-09-30 01:32:19 +0530

    error

--- src/components/contact/contact.jsx
+++ src/components/contact/contact.jsx
@@ -4,15 +4,7 @@ import './contact.css'
 const Contact = () => {
   return (
     <div>
-		{/* <div class='light x1'></div>
-  <div class='light x2'></div>
-  <div class='light x3'></div>
-  <div class='light x4'></div>
-  <div class='light x5'></div>
-  <div class='light x6'></div>
-  <div class='light x7'></div>
-  <div class='light x8'></div>
-  <div class='light x9'></div> */}
+	
    <div id="stars"></div>
         <div id="stars2"></div> 
         <div id="stars3"></div>
